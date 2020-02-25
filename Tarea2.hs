3c694a850b68954a09d2d35870eb6f5bb6234a06
DanyLugs <danylugo@ciencias.unam.mx>
2020-02-25 13:21:45 -0600

    2da tarea

---
+++ Tarea2.hs
@@ -0,0 +1,20 @@
+module Tarea2 where
+
+import Data.Bits
+​
+gtrPower2:: Int -> Int
+gtrPower2 n = if popCount (n-1) == 1
+              then n-1
+              else  gtrPower2 (n-1)
+
+​
+
+inarow :: (Eq a) => [a] -> Int
+inarow [] = 0
+inarow (x:xs) = inarow_aux (1,1,x) xs
+ 
+ ramanujan :: Int -> [(Int,Int,Int,Int)]
+ ramanujan n = [(a,b,c,d) | a <- [1..n], b <- [a..n],
+                            c <- [a+1..n], d <- [c..n],
+                            a^3 + b^3 == c^3 + d^3]
+                            
